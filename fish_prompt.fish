5334d486efe944492d2313cf232f1ca0bdbbea8e
Claudio Mezzasalma <claudio.mezzasalma@gmail.com>
2019-01-30 09:08:43 +0100

    Add space after ahead and behind in git prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -270,11 +270,11 @@ function __bobthefish_git_ahead_verbose -S -d 'Print a more verbose ahead/behind
         case '0 0' # equal to upstream
             return
         case '* 0' # ahead of upstream
-            echo "$git_ahead_glyph$ahead"
+            echo "$git_ahead_glyph $ahead"
         case '0 *' # behind upstream
             echo "$git_behind_glyph$behind"
         case '*' # diverged from upstream
-            echo "$git_ahead_glyph$ahead$git_behind_glyph$behind"
+            echo "$git_ahead_glyph $ahead$git_behind_glyph $behind"
     end
 end
 
